447d3405ce89c3f51d063e0a3b7fe40fc9276ba1
KrisMuSs <artem.merzlikin.67@mail.ru>
2026-03-15 22:16:01 +0700

    Добавлена простая реализация board() для прохождения теста размера строк

--- Game512_TDD/GameLogic.swift
+++ Game512_TDD/GameLogic.swift
@@ -2,5 +2,11 @@
 import Foundation
 
 final class GameLogic: ObservableObject {
-    
+    //todo временно задали поле вручную, позже переделать логику инициализации
+    @Published private(set) var board: [[Int]] = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
 }
