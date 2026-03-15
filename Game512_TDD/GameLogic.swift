bbe209dd375e0c423f132cb2a821ccd49b0ff854
KrisMuSs <artem.merzlikin.67@mail.ru>
2026-03-16 02:00:03 +0700

    Добавлена простая реализация move() для прохождения теста с right

--- Game512_TDD/GameLogic.swift
+++ Game512_TDD/GameLogic.swift
@@ -6,7 +6,7 @@ protocol TileSpawner {
 }
 
 enum Direction {
-    case left
+    case left, right
 }
 
 struct StartTileSpawner: TileSpawner {
@@ -58,6 +58,13 @@ final class GameLogic: ObservableObject {
                 score += gainedTotal
                 spawner.spawn(on: &board)
             }
+        case .right:
+            //todo временная реализация: пока обрабатываем только первый тест для первой строки
+            // позже переписать на общую логику хода вправо
+            if board[0] == [2, 0, 4, 0] {
+                board[0] = [0, 0, 2, 4]
+            }
+            
         }
     }
     
